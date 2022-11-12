7fa9e93d7805fc4799617319d8f3181ccd545855
Stephanie Calderon <sncalderon3@gmail.com>
2022-11-12 15:41:22 +1100

    bulk commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'restaurants', to: 'restaurants#index', as: :restaurants
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post '/restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
+  # get '/restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create, :show]
+  end
+  resources :reviews, only: [:create, :new]
 end
